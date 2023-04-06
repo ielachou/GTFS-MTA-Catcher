146c1c7e976e80b449f941642a70234d75957019
Your Name <you@example.com>
2023-04-06 17:11:08 +0200

    fix try except

--- data_catcher.py
+++ data_catcher.py
@@ -21,25 +21,28 @@ if not os.path.exists("res/bus"):
         os.makedirs('res/bus')
 i = -1
 while i < 40320:
-    i+=1
-    now = datetime.now()
+    try:
+        i+=1
+        now = datetime.now()
 
-    dt_string = now.strftime("%Y-%m-%d_%H-%M-%S")
-    for feed in feeds:
-        x = requests.get('https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/' + feed, headers={'x-api-key' : key})    
-        if feed == "nyct%2Fgtfs":
-            term = "gtfs"
-        else:
-            term = feed.split('-')[1]
-        f = open("res/"+term+"/ny_mta_gtfs_"+term+"_"+dt_string, 'wb')
+        dt_string = now.strftime("%Y-%m-%d_%H-%M-%S")
+        for feed in feeds:
+            x = requests.get('https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/' + feed, headers={'x-api-key' : key})    
+            if feed == "nyct%2Fgtfs":
+                term = "gtfs"
+            else:
+                term = feed.split('-')[1]
+            f = open("res/"+term+"/ny_mta_gtfs_"+term+"_"+dt_string, 'wb')
+            compressed_text = zlib.compress(x.content)
+            f.write(compressed_text)
+            f.close()
+        
+        x = requests.get( 'http://gtfsrt.prod.obanyc.com/vehiclePositions?key='+key)
+        f = open("res/bus/ny_mta_gtfs_bus_" + dt_string, 'wb')
         compressed_text = zlib.compress(x.content)
         f.write(compressed_text)
         f.close()
-    
-    x = requests.get( 'http://gtfsrt.prod.obanyc.com/vehiclePositions?key='+key)
-    f = open("res/bus/ny_mta_gtfs_bus_" + dt_string, 'wb')
-    compressed_text = zlib.compress(x.content)
-    f.write(compressed_text)
-    f.close()
 
-    time.sleep(15)
+        time.sleep(15)
+    except:
+        pass
